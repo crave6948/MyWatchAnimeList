12c2a97e12ca262be5fa60e1d348a035895226db
crave6948 <59335159+crave6948@users.noreply.github.com>
2024-02-18 16:18:25 +0700

    Refactor ActionMenu component to include a toggleable add anime button

--- vue/src/components/ActionMenu.vue
+++ vue/src/components/ActionMenu.vue
@@ -1,11 +1,16 @@
 <template>
-  <div id="buttonContainer">
-    <button @click="clicked">Add Anime</button>
+  <div id="stickyDiv">
+    <img @click="open = !open" id="icon" src="../assets/plusicon.svg" alt="plus">
   </div>
-  <div id="panel">
-    <input type="text" v-model="animeName" placeholder="Anime Name" />
-    <input type="number" v-model="animeWatch" placeholder="Watched Episodes" />
-    <input type="text" v-model="animeImage" placeholder="Anime Image" />
+  <div v-if="open">
+    <div id="buttonContainer">
+      <button @click="clicked">Add Anime</button>
+    </div>
+    <div id="panel">
+      <input type="text" v-model="animeName" placeholder="Anime Name" />
+      <input type="number" v-model="animeWatch" placeholder="Watched Episodes" />
+      <input type="text" v-model="animeImage" placeholder="Anime Image" />
+    </div>
   </div>
 </template>
 
@@ -16,7 +21,8 @@ export default {
     return {
       animeName: '',
       animeWatch: 0,
-      animeImage: ''
+      animeImage: '',
+      open: false
     }
   },
   methods: {
@@ -28,15 +34,28 @@ export default {
 </script>
 
 <style>
+#icon {
+  width: 50px;
+  height: 50px;
+  border-radius: 12px;
+  position: relative;
+}
+
+#stickyDiv {
+  position: fixed;
+  bottom: 5%;
+  right: 5%;
+}
+
 #buttonContainer {
   display: flex;
   justify-content: center;
   margin-top: 20px;
 }
+
 #panel {
   display: flex;
   flex-direction: column;
   align-items: center;
   margin-top: 20px;
-}
-</style>
+}</style>
